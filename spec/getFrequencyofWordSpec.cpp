57c321bb9496132cc6293afa66aed4fb7740ffb4
subhamsahoo <subhamsahoo@missionrnd.com>
2016-03-27 14:57:00 +0530

    Submit Commit

--- spec/getFrequencyofWordSpec.cpp
+++ spec/getFrequencyofWordSpec.cpp
@@ -15,7 +15,7 @@ namespace spec
 		{
 			char sentences[][100] = { "abababa", "he he", "ab cd ab cd", "    ", "1234 aaaaaaa", "asd324", "yzqwer 23425", "a" };
 			char words[][100] = { "ab", "he", "ab cd", " ", "aa", "asd324", "x", "a" };
-			int counts[100] = { 3,2,2,4,6,1,0};
+			int counts[100] = { 3,2,2,4,3,1,0};
 			int sc = 7;
 			int si = 0;
 			for (si = 0; si < sc; si++){
--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -13,7 +13,25 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 #include <stdlib.h>
 
 int count_word_in_str_way_1(char *str, char *word){
-	return 0;
+	int count = 0, j = 0, count1 = 0;
+	for (int i = 0; word[i] != '\0'; i++)
+		count1++;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+
+		if (str[i] == word[j])
+		{
+			j++;
+			if (j == count1){
+				count++;
+				j = 0;
+			}
+		}
+		else if (str[i] != word[j])
+			j = 0;
+	}
+	return count;
 }
 
 int count_word_int_str_way_2_recursion(char *str, char *word){
